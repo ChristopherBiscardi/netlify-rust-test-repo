93672b71c0642f8f0056d4714cd14d7708ad949a
Christopher Biscardi <chris@christopherbiscardi.com>
2021-09-12 20:16:01 -0700

    ship

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use lambda_http::{Body, IntoResponse, Response};
 use lambda_runtime::{handler_fn, Context, Error};
 use serde_json::{json, Value};
 
@@ -14,9 +13,11 @@ async fn handler(
     event: Value,
     _: Context,
 ) -> Result<Value, Error> {
-    dbg!("in main", &event.to_string());
-    let first_name =
-        event["firstName"].as_str().unwrap_or("world");
+    dbg!("in main", &event.as_str());
+    let first_name = event["queryStringParameters"]
+        ["firstName"]
+        .as_str()
+        .unwrap_or("world");
 
     dbg!(&first_name);
     Ok(json!({
